59f60c5236add033e8c5cb4833596cc006f0721b
5calV <110572069+5calV@users.noreply.github.com>
2022-10-29 21:18:22 +0200

    Add files via upload

---
+++ clone_everything.sh
@@ -0,0 +1,69 @@
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#   ╭∩╮     ╭∩╮
+#    \(͡⚈ᴗ͡⚈)/
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Folder
+
+echo "The cloned repositorys will be in Folder 5calV"
+echo "The cloned repositorys will be in Folder 5calV"
+echo "The cloned repositorys will be in Folder 5calV"
+echo "The cloned repositorys will be in Folder 5calV"
+echo "The cloned repositorys will be in Folder 5calV"
+
+mkdir 5calV
+
+cd 5calV
+
+# Start Message
+
+echo "Starting to clone EVERY Git Repository of 5calV"
+
+sleep 1
+
+echo "Starting to clone EVERY Git Repository of 5calV"
+
+sleep 1
+
+echo "Starting to clone EVERY Git Repository of 5calV"
+
+sleep 1
+
+echo "Starting to clone EVERY Git Repository of 5calV"
+
+sleep 1
+
+echo "Starting to clone EVERY Git Repository of 5calV"
+
+echo "https://github.com/5calV"
+echo "https://github.com/5calV"
+echo "https://github.com/5calV"
+echo "https://github.com/5calV"
+echo "https://github.com/5calV"
+
+sleep 2
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+# Python / Tkinter Password Generator
+
+echo "Cloning Python / Tkinter Password Generator"
+
+sleep 1
+
+echo "Cloning Python / Tkinter Password Generator"
+
+sleep 1
+
+echo "Cloning Python / Tkinter Password Generator"
+
+sleep 1
+
+git clone https://github.com/5calV/Tkinter-Password-Generator.git
+
+sleep 3
+
+#-----------------------------------------------------------------------------------------------------------------------------------
